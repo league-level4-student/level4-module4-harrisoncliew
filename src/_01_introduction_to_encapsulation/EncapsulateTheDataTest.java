420c5e73b478cce15d973882705ab77abf89626f
harrisoncliew <41922141+harrisoncliew@users.noreply.github.com>
2020-07-22 11:31:07 -0700

    EncapsulateTheData, Polymorphism(IP)

--- src/_01_introduction_to_encapsulation/EncapsulateTheDataTest.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheDataTest.java
@@ -4,25 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-/*
- * Encapsulation is a way of protecting the data in a class from being
- * unintentionally altered from another class.
- * 
- * To encapsulate a member variable, follow these three steps:
- * 
- * 1. Make the member variable private (or protected)
- * 
- * 2. Make a getter and setter method (accessor and mutator) for the variable.
- * 
- * 3. Add restrictions to the setter method so the member variable cannot be 
- * 	  adversely altered
- * 
- * */
+class EncapsulateTheDataTest {
 
-
-public class EncapsulateTheData {
-	//1. Encapsulate the member variables.
-	//   Add restrictions to the setters according to the comment.
 	public int getItemsReceived() {
 		return itemsReceived;
 	}
@@ -99,20 +82,11 @@ public class EncapsulateTheData {
 		setMemberObj(20);
 		assertEquals(20, getMemberObj());
 		setMemberObj("This is a string");
-		assertEquals(new Object(), getMemberObj());
+		assertEquals(false, (getMemberObj() instanceof String));
 	}
 	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
 	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
 	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
 	private Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
 	
-	public static void main(String[] args) {
-		
-//		EncapsulateTheData e = new EncapsulateTheData();
-//		e.testItemsReceived();
-//		e.testDegreesTurned();
-//		e.testMemberObj();
-//		e.testNomenclature();
-		
-	}
 }
